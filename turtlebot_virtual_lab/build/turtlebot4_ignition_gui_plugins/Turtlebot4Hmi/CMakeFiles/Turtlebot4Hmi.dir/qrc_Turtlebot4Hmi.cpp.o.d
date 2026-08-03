e4ca6707133c63209ca2ac58ef7b6499fe69d21b
robotai <robotai@V-2204-HPLC-11.staff.ad.curtin.edu.au>
2026-08-03 17:18:00 +0800

    Finished Lab 1

---
+++ turtlebot_virtual_lab/build/turtlebot4_ignition_gui_plugins/Turtlebot4Hmi/CMakeFiles/Turtlebot4Hmi.dir/qrc_Turtlebot4Hmi.cpp.o.d
@@ -0,0 +1,3 @@
+Turtlebot4Hmi/CMakeFiles/Turtlebot4Hmi.dir/qrc_Turtlebot4Hmi.cpp.o: \
+ /home/robotai/AutoRobots/turtlebot_virtual_lab/build/turtlebot4_ignition_gui_plugins/Turtlebot4Hmi/qrc_Turtlebot4Hmi.cpp \
+ /usr/include/stdc-predef.h
---
+++ turtlebot_virtual_lab/build/turtlebot4_ignition_gui_plugins/Turtlebot4Hmi/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robotai/AutoRobots/turtlebot_virtual_lab/src/turtlebot4_simulator/turtlebot4_ignition_gui_plugins/Turtlebot4Hmi
+# Build directory: /home/robotai/AutoRobots/turtlebot_virtual_lab/build/turtlebot4_ignition_gui_plugins/Turtlebot4Hmi
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
