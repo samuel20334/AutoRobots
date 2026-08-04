f981fd46e938c6c1420ffd9bbbb98103c9ecf379
Samuel Petrovic <smpetrovic131@gmail.com>
2026-08-04 08:56:55 +0800

    Deleted virtual lab

--- turtlebot_virtual_lab/build/turtlebot4_ignition_bringup/colcon_command_prefix_build.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/robotai/AutoRobots/turtlebot_virtual_lab/install/turtlebot4_ignition_gui_plugins/share/turtlebot4_ignition_gui_plugins/package.sh"
-. "/home/robotai/AutoRobots/turtlebot_virtual_lab/install/turtlebot4_ignition_toolbox/share/turtlebot4_ignition_toolbox/package.sh"
--- turtlebot_virtual_lab/build/turtlebot4_simulator/colcon_command_prefix_build.sh
+++
@@ -1,4 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/robotai/AutoRobots/turtlebot_virtual_lab/install/turtlebot4_ignition_gui_plugins/share/turtlebot4_ignition_gui_plugins/package.sh"
-. "/home/robotai/AutoRobots/turtlebot_virtual_lab/install/turtlebot4_ignition_toolbox/share/turtlebot4_ignition_toolbox/package.sh"
-. "/home/robotai/AutoRobots/turtlebot_virtual_lab/install/turtlebot4_ignition_bringup/share/turtlebot4_ignition_bringup/package.sh"
