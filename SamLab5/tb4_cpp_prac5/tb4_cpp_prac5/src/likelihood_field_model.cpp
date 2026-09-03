f1e0920269c14e834314c79d94223d4905705607
Samuel Petrovic <smpetrovic131@gmail.com>
2026-09-03 10:34:24 +0800

    Completed Lab 5, untested

---
+++ SamLab5/tb4_cpp_prac5/tb4_cpp_prac5/src/likelihood_field_model.cpp
@@ -0,0 +1,130 @@
+#include <math.h>
+#include <assert.h>
+
+#include "iar_amcl/likelihood_field_model.hpp"
+
+namespace iar_amcl
+{
+    LikelihoodFieldModel::LikelihoodFieldModel(
+    double z_hit, double z_max, double z_rand, double sigma_hit,
+    double max_occ_dist, size_t max_beams, map_t * map)
+    : Laser(max_beams, map)
+    {
+        z_hit_ = z_hit;
+        z_rand_ = z_rand;
+        sigma_hit_ = sigma_hit;
+        z_max_ = z_max;
+        map_update_cspace(map, max_occ_dist);
+    }
+
+    // Determine the probability for the given pose
+    double
+    LikelihoodFieldModel::sensorFunction(nav2_amcl::LaserData * data, pf_sample_set_t * set)
+    {
+        LikelihoodFieldModel * self;
+        int i, j, step;
+        double pz; 
+        double dist;
+        double p; // probability (or weight) of indivdual particle
+        double obs_range, obs_bearing; // the measurement values of one laser beam
+        double total_weight;
+        pf_sample_t * sample;
+        pf_vector_t pose_robot; 
+
+        // hit_x and hit_y are the horizontal position and vertical position of the endpoint of a laser beam
+        double hit_x, hit_y;
+
+        self = reinterpret_cast<LikelihoodFieldModel *>(data->laser);
+
+        total_weight = 0.0;
+
+        // Compute the sample weights
+        for (j = 0; j < set->sample_count; j++) {
+                sample = set->samples + j;
+                pose_robot = sample->pose;
+
+                /* TODO TASK - MILSTONE 1.1 
+                    Compute the pose of the lidar sensor.
+                */
+                pf_vector_t lidar_pose_to_robot = self->laser_pose_;
+                pf_vector_t lidar_pose = pf_vector_coord_add(lidar_pose_to_robot, pose_robot);
+
+
+                p = 1.0;
+
+                step = (data->range_count - 1) / (self->max_beams_ - 1);
+                // Step size normally larger than 1, but in case of error setting max number beams used 
+                // to model the probability, we need to check the step and ensure it is larger or equal to 1
+                if (step<1)
+                    step = 1;
+
+                for (i = 0; i < data->range_count; i += step) {
+                    obs_range = data->ranges[i][0];
+                    obs_bearing = data->ranges[i][1];
+
+                    pz = 0.0;
+                    /* TODO TASK - MILESTONE 1.2
+                        Check whether the laser beam's value is infinity, if yes, skip this beam measurement
+                    */
+                    if (std::isnan(obs_range)) {
+                        continue;
+                    }
+
+                    /* 
+                        TODO TASK - MILESTONE 1.3
+                        Check whether a failure measurement is detected, i.e., max range is detected. If yes, update the probability
+                    */
+                    if (obs_range == data->range_max) {
+                        pz = pz + z_max_;
+                    }
+
+
+                    /* 
+                        TODO TASK - MILESTONE 1.4
+                        Process a beam with range measurement less than the maximum value.
+                    */
+                    else if (obs_range < data->range_max) {
+                        pz = pz + z_rand_/data->range_max;
+                        hit_x = lidar_pose.v[0] + obs_range*cos(lidar_pose.v[2] + obs_bearing);
+                        hit_y = lidar_pose.v[1] + obs_range*sin(lidar_pose.v[2] + obs_bearing);
+
+                        int m_x = MAP_GXWX(self->map_, hit_x);
+                        int m_y = MAP_GYWY(self->map_, hit_y);
+                        int index = MAP_INDEX(self->map_, m_x, m_y);
+                        bool valid = MAP_VALID(self->map_, m_x, m_y);
+
+                        if (valid) {
+                            dist = self->map_->cells[index].occ_dist;
+                        } else {
+                            dist = self->map_->max_occ_dist;
+                        }
+
+                        pz = pz + z_hit_*exp(-(dist*dist)/(2*sigma_hit_*sigma_hit_));
+                    }
+
+                    
+                    
+                    assert(pz <= 1.0);
+                    assert(pz >= 0.0);
+                    //      p *= pz;
+                    // here we have an ad-hoc weighting scheme for combining beam probs
+                    // works well, though...
+                    p += pz * pz * pz;
+                }
+            sample->weight *= p;
+            total_weight += sample->weight;
+        }
+        return total_weight;
+    }
+
+    bool 
+    LikelihoodFieldModel::sensorUpdate(pf_t * pf, nav2_amcl::LaserData * data)
+    {
+        if (max_beams_ < 2) {
+            return false;
+        }
+        pf_update_sensor(pf, (pf_sensor_model_fn_t) sensorFunction, data);
+
+        return true;
+    }
+}
